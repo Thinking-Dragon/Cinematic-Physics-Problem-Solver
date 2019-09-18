f0cdc37f494d23b5b8838744411f7e3611526f65
Clément Gassmann-Prince <cagap8@gmail.com>
2019-09-18 13:11:05 -0400

    Created project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cinematic_Physics_Problem_Solver.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
